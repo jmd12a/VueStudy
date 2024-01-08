6ad0250447fe494a646c4ff35df0485ea6f185e2
layne <928814697@qq.com>
2024-01-08 20:05:05 +0800

    第一次上传，准备学toList案例

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,49 @@
+<script>
+import {nanoid} from "nanoid";
+
+export default {
+  name: "MyHeader",
+  props:{
+    todos:Array
+  },
+  data(){
+    return{
+      title:''
+    }
+  },
+
+  methods:{
+    add(){
+      if (this.title.trim() === '') return
+      const obj = {id:nanoid(), name:this.title, completed:false}
+      // this.todos = []
+      this.todos.unshift(obj) // 通过props传入的属性不能直接操作赋值，因为不能直接改变内存地址，但是可以对这个值本身进行一些操作
+      this.title=''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="todo-header">
+    <input type="text" placeholder="请输入你的任务名称 按回车键确认" v-model="title" @keyup.enter="add"/>
+  </div>
+</template>
+
+<style scoped>
+    /*header*/
+    .todo-header input {
+      width: 560px;
+      height: 28px;
+      font-size: 14px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      padding: 4px 7px;
+    }
+
+    .todo-header input:focus {
+      outline: none;
+      border-color: rgba(82, 168, 236, 0.8);
+      box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+    }
+</style>
---
+++ src/components/MyItem.vue
@@ -0,0 +1,80 @@
+<script>
+export default {
+  name: "MyItem",
+  props:{
+    todoObj:Object,
+    removeTodo:Function
+  },
+  methods:{
+    /*removeTodo(todoObj){
+      // 不能直接改变父组件传进来的值(即不能将值的内存地址指向其他地方)，但可以改变传进来的值的子属性的值，违反了单向数据流原则暂时这样用
+      todoObj.completed= !todoObj.completed
+    }*/
+
+    handlerDelete(id){
+      this.removeTodo(id)
+    }
+  }
+}
+</script>
+
+<template>
+  <li>
+    <label>
+<!--      : + 标签名，后面的值为布尔值，来动态的决定便签属性-->
+<!--      <input type="checkbox" :checked="todoObj.completed" @change="removeTodo(todoObj)"/>-->
+<!--      在这里，v-model绑定todoObj的completed-属性，实现了双向数据绑定，completed决定了这个输入框是否显示 并且当我们改变数据框的状态时
+          会反过来影响todoObj的completed属性，todoOb在app中存储，但这里通过引用修改了在app中它的属性的值
+-->
+      <input type="checkbox" v-model="todoObj.completed">
+      <span>{{todoObj.name}}</span>
+    </label>
+    <button class="btn btn-danger" @click="handlerDelete(todoObj.id)">删除</button>
+  </li>
+
+</template>
+
+<style scoped>
+    /*item*/
+    li {
+      list-style: none;
+      height: 36px;
+      line-height: 36px;
+      padding: 0 5px;
+      border-bottom: 1px solid #ddd;
+    }
+
+    li label {
+      float: left;
+      cursor: pointer;
+    }
+
+    li label li input {
+      vertical-align: middle;
+      margin-right: 6px;
+      position: relative;
+      top: -1px;
+    }
+
+    li button {
+      float: right;
+      display: none;
+      margin-top: 3px;
+    }
+
+    li:before {
+      content: initial;
+    }
+
+    li:last-child {
+      border-bottom: none;
+    }
+
+    li:hover{
+      background-color: #ddd;
+    }
+
+    li:hover button{
+      display: block;
+    }
+</style>
